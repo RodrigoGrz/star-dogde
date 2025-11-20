eb392802c980206b648afc541db8109e13eaa841
Rodrigo Gregorio <rodrigo.gregorio1@hotmail.com>
2025-11-20 13:45:39 -0300

    fix: system ordering

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -11,8 +11,7 @@ impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app
             .add_systems(Startup, spawn_player)
-            .add_systems(Update, player_movement)
-            .add_systems(Update, confine_player_movement)
+            .add_systems(Update, (player_movement, confine_player_movement.after(player_movement)))
             .add_systems(Update, player_hit_stars)
             .add_systems(Update, enemy_hit_player);
     }
